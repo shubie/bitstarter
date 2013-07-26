3e7e31eacd2b09730e2e46d020709986a9096049
shuaib shubby <babasasworld@yahoo.com>
2013-07-26 16:02:19 +0100

    web.js reading from index

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var htmlfile = "index.html";
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
